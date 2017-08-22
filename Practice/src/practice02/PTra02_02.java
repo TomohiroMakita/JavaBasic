5c7af1ec36a2fdd516072deebbe6206045fa25d2
TomohiroMakita <mrsgreel@gmail.com>
2017-08-22 15:48:44 +0900

    Ptra02_10 まで完了

--- Practice/src/practice02/PTra02_02.java
+++ Practice/src/practice02/PTra02_02.java
@@ -13,6 +13,8 @@ public class PTra02_02 {
 
 		// ★ 変数numの値に30足した数を出力してください
 
+		num += 30;
+		System.out.println(num);
 
 		// ★ 以下のプログラムで40が出力されるようにしてください
 		System.out.println(num);	// ※※ この行は修正しないでください
