4b4edd3a20e4a42db8f7ad3e0bd169601f10a233
DESKTOP-JKKPCO4\likeit_student2 <mrsgreel@ezweb.ne.jp>
2017-09-22 18:40:53 +0900

    17-4まで完了

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -16,5 +16,20 @@ public class PTra17_04 {
 		 * ※ Thread.sleep を使用すること
 		 */
 
+		char data[] = str.toCharArray();
+
+		int arr_num = data.length;
+
+		for (int i = 0; i <= arr_num-1; i++) {
+
+		try{
+			  Thread.sleep(1000);
+
+			}catch (InterruptedException e){
+			}
+
+		System.out.print(data[i]);
+
+		}
 	}
 }
